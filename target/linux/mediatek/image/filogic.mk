87ebf915967fcba4dc2b2b74dcb9c7327854a8fa
SuperKali <hello@superkali.me>
2025-08-28 20:08:13 +0200

    filogic: add first support to wirelesstag wt7981n
    Signed-off-by: SuperKali <hello@superkali.me>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2263,3 +2263,21 @@ ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
 endif
 endef
 TARGET_DEVICES += wirelesstag_zx7981poeq-ubootmod
+
+define Device/wirelesstag_wt7981n
+  DEVICE_VENDOR := Wireless-Tag
+  DEVICE_MODEL := WT7981N
+  DEVICE_DTS := mt7981b-wirelesstag-wt7981n
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware kmod-usb3 mt7981-wo-firmware
+  DEVICE_DTS_LOADADDR := 0x44000000
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 51200k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += wirelesstag_wt7981n
